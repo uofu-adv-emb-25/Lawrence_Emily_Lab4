496d84afdce1d3243a270750b986217b9789b6fd
Emily Yang <u0835353@utah.edu>
2025-09-24 13:00:25 -0600

    Implemented worker handle for thread pool and work queue activity.

---
+++ src/fifo.c
@@ -0,0 +1,19 @@
+#include "fifo.h"
+
+void fifo_worker_handler(QueueHandle_t requests, QueueHandle_t results, int id)
+{
+    while(1) 
+    {
+        //Get task off request queue
+        struct request_msg *data = {};
+        xQueueReceive(requests, &data, portMAX_DELAY);
+
+        //Perform work
+        data->output = data->input+5;
+        data->handled_by = id;
+
+        //Place finished work on results queue
+        xQueueSendToBack(results, &data, portMAX_DELAY);
+    }
+
+}
