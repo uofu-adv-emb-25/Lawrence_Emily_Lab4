87911a9845c19c842485c84fb21f6bf64b2d5746
Emily Yang <u0835353@utah.edu>
2025-09-29 13:05:52 -0600

    We made a small mistake with how data was passed by address and thought we needed to dereference. Fixed it, now the tests run.

--- src/fifo.c
+++ src/fifo.c
@@ -5,12 +5,12 @@ void fifo_worker_handler(QueueHandle_t requests, QueueHandle_t results, int id)
     while(1) 
     {
         //Get task off request queue
-        struct request_msg *data = {};
+        struct request_msg data = {};
         xQueueReceive(requests, &data, portMAX_DELAY);
 
         //Perform work
-        data->output = data->input+5;
-        data->handled_by = id;
+        data.output = data.input+5;
+        data.handled_by = id;
 
         //Place finished work on results queue
         xQueueSendToBack(results, &data, portMAX_DELAY);
